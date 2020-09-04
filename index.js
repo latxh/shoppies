2a88072abf967b7e6f3e77c8ac9158c3ad763524
Latxhman <55321722+Latxhman@users.noreply.github.com>
2020-09-04 14:28:55 -0400

    Working Version. Needs cleaning of CSS and JS.

--- index.js
+++ index.js
@@ -9,24 +9,16 @@ let cardPlaceholderElm = document.getElementById('cardPlaceholder');
 
 let nominations = [null, null, null, null, null];
 
-const popup = document.querySelector('.full-screen');
-console.log(popup);
-
-function togglePopup() {
-    popup.classList.toggle('hidden');
-
-}
-
-
 async function search(first) {
-    console.log("Searched", first, searchElm.value);
+
+    // console.log("Searched", first, searchElm.value);
 
     let response = { "Search": [{ "Title": "The Invisible Man", "Year": "2020", "imdbID": "tt1051906", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BZjFhM2I4ZDYtZWMwNC00NTYzLWE3MDgtNjgxYmM3ZWMxYmVmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg" }, { "Title": "The Old Guard", "Year": "2020", "imdbID": "tt7556122", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BNDJiZDliZDAtMjc5Yy00MzVhLThkY2MtNDYwNTQ2ZTM5MDcxXkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_SX300.jpg" }, { "Title": "Sonic the Hedgehog", "Year": "2020", "imdbID": "tt3794354", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BMDk5Yzc4NzMtODUwOS00NTdhLTg2MjEtZTkzZjc0ZWE2MzAwXkEyXkFqcGdeQXVyMTA3MTA4Mzgw._V1_SX300.jpg" }, { "Title": "Eurovision Song Contest: The Story of Fire Saga", "Year": "2020", "imdbID": "tt8580274", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BYzRjYzA5NTQtOTE3MC00OTYzLWEzODItMzQxYWE1NDJkMDA0XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg" }, { "Title": "The Hunt", "Year": "2020", "imdbID": "tt8244784", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BNjg4MjRhZjgtNTIxOS00MmRjLTg4NTEtNjBkNzkwZjAxMjMyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg" }, { "Title": "The Call of the Wild", "Year": "2020", "imdbID": "tt7504726", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BZDA1ZmQ2OGMtZDhkMC00ZjRkLWE3ZTMtMzA5ZTk0YjM1OGRmXkEyXkFqcGdeQXVyNzI1NzMxNzM@._V1_SX300.jpg" }, { "Title": "The Wrong Missy", "Year": "2020", "imdbID": "tt9619798", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BY2QwZWJlZjMtNzU5NC00NTA0LWI1MjQtYWQ1ZTg4NWZmNjdkXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_SX300.jpg" }, { "Title": "The Way Back", "Year": "2020", "imdbID": "tt8544498", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BYjBjMTgyYzktN2U0Mi00YTJhLThkZDQtZmM1ZDlmNWMwZDQ3XkEyXkFqcGdeQXVyMDU5MDEyMA@@._V1_SX300.jpg" }, { "Title": "The Half of It", "Year": "2020", "imdbID": "tt9683478", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BY2RlZmZkOTUtMDI5Ni00ZjZmLWI1OTItZmUwNWE4ZWVjNzFiXkEyXkFqcGdeQXVyMTkzODUwNzk@._V1_SX300.jpg" }, { "Title": "To All the Boys: P.S. I Still Love You", "Year": "2020", "imdbID": "tt9354842", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BZjMwNDQ4NzMtOThmZi00NmMyLThkMWItMTA3MTg2YjdiZDRmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg" }], "totalResults": "2102", "Response": "True" };
-    console.log(response.Search.length);
+    // console.log(response.Search.length);
     if (!first) {
-        // response = await (await fetch(`http://www.omdbapi.com/?apikey=1e95d568&type=movie&s=${searchElm.value}`, {
-        //     cache: 'force-cache'
-        // })).json();
+        response = await (await fetch(`http://www.omdbapi.com/?apikey=1e95d568&type=movie&s=${searchElm.value}`, {
+            cache: 'force-cache'
+        })).json();
         console.log(first);
     }
 
@@ -40,7 +32,7 @@ async function search(first) {
         resultsElm.textContent = ((response.Search.length === 0 || !response.totalResults) ? 'No ' : response.totalResults + " ") + `Results for "${searchElm.value}"`;
     }
 
-    console.log("Response", response);
+    // console.log("Response", response);
 
     for (const movie of response.Search) {
         let nominated = nominations.find(a => a && a.imdbID === movie.imdbID);
@@ -70,7 +62,16 @@ async function search(first) {
         elm.getElementsByClassName('year')[0].textContent = "RELEASED " + movie.Year;
         elm.getElementsByClassName('poster')[0].src = movie.Poster;
 
-        elm.addEventListener('click', () => { nominateClick(elm, movie) });
+        // elm.addEventListener('click', () => { nominateClick(elm, movie) });
+
+        // remove previously binded event listener;
+        elm.removeEventListener('click', nominateClickHandler, false);
+
+        // add the new listener;
+        elm.addEventListener('click', nominateClickHandler, false);
+
+        // set data to be fetched on click event;
+        elm.setAttribute("data-movie", btoa(JSON.stringify(movie)))
 
         setTimeout(() => {
             elm.classList.add('visible');
@@ -78,7 +79,18 @@ async function search(first) {
     }
 }
 
+var nominateClickHandler = function (event) {
+    // event.srcElements gives card element but we need its parent hence the parent node is taken;
+    let elm = event.srcElement.closest(".movie-card-containter");
+    let movie = elm.getAttribute("data-movie") ? atob(elm.getAttribute("data-movie")) : "{}";
+    movie = JSON.parse(movie);
+
+    // nominate the movie;
+    nominateClick(elm, movie);
+}
+
 function nominateClick(elm, movie) {
+
     let nominatedInd = nominations.findIndex(a => a && a.imdbID === movie.imdbID);
     if (nominatedInd !== -1) { return; }
 
@@ -88,7 +100,17 @@ function nominateClick(elm, movie) {
 
     elm.classList.remove('visible');
     let clone = elm.cloneNode(true);
-    clone.addEventListener('click', () => { nominatedClick(clone, movie) });
+    // clone.addEventListener('click', () => { nominatedClick(clone, movie) });
+
+    // remove previously binded event listener;
+    clone.removeEventListener('click', nominatedClickHandler, false);
+
+    // add the new listener;
+    clone.addEventListener('click', nominatedClickHandler, false);
+
+    // set data to be fetched on click event;
+    clone.setAttribute("data-movie", btoa(JSON.stringify(movie)))
+
     clone.getElementsByClassName('nominate')[0].textContent = "NOMINATED";
 
     setTimeout(() => {
@@ -107,7 +129,20 @@ function nominateClick(elm, movie) {
     }
 }
 
+
+var nominatedClickHandler = function (event) {
+    // event.srcElements gives card element but we need its parent hence the parent node is taken;
+    let clone = event.srcElement.closest(".movie-card-containter");
+    let movie = clone.getAttribute("data-movie") ? atob(clone.getAttribute("data-movie")) : "{}";
+    movie = JSON.parse(movie);
+
+    // nominate the movie;
+    nominatedClick(clone, movie);
+}
+
 function nominatedClick(elm, movie) {
+    console.log("nominations -> ", nominations);
+    console.log("movie ->", movie);
     let nominatedInd = nominations.findIndex(a => a && a.imdbID === movie.imdbID);
     if (nominatedInd === -1) { return; }
 
@@ -115,7 +150,18 @@ function nominatedClick(elm, movie) {
 
     elm.classList.remove('visible');
     let clone = elm.cloneNode(true);
-    clone.addEventListener('click', () => { nominateClick(clone, movie) });
+
+    // clone.addEventListener('click', () => { nominateClick(clone, movie) });
+
+    // remove previously binded event listener
+    clone.removeEventListener('click', nominateClickHandler, false);
+
+    // add the new listener
+    clone.addEventListener('click', nominateClickHandler, false);
+
+    // set data to be fetched on click event
+    clone.setAttribute("data-movie", btoa(JSON.stringify(movie)))
+
     clone.getElementsByClassName('nominate')[0].textContent = "NOMINATE";
 
     setTimeout(() => {
